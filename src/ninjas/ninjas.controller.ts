53a466fcfa7c795dd04f6c9efb36bce0c4f95614
mather <misaka@misaka.im>
2023-03-24 11:13:04 +0800

    add ninjas controller resful api

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { UpdateNinjaDto }                                         from './dto/update-ninja.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  @Get()
+  getNinjas(@Query('type') type: string){
+    return [{ type }]
+  }
+
+  @Get()
+  getOneNinjas(@Param('id') id: string){
+    return {
+      id,
+    }
+  }
+
+  @Post()
+  creatNinja(@Body() creatNinjaDto){
+    return {
+      name: creatNinjaDto.name
+    }
+  }
+
+  @Put(':id')
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto){
+    return {
+      id,
+      name: updateNinjaDto.name
+    }
+  }
+
+  @Delete(':id')
+  removeNinja(@Param('id') id: string) {
+    return {
+      id
+    }
+  }
+
+
+}
